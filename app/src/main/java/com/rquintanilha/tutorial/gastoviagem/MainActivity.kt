544748299684b41488ba8c92c14df048cb8a9a5a
rodolfq <Amanda@45>
2022-11-03 12:35:24 -0300

    v1.0

---
+++ app/src/main/java/com/rquintanilha/tutorial/gastoviagem/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.rquintanilha.tutorial.gastoviagem
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.rquintanilha.tutorial.gastoviagem.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+
+        // val mTextView = findViewById<TextView>(R.id.text_link)
+        //mTextView.movementMethod = LinkMovementMethod.getInstance()
+
+
+
+        //
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValidate(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonnomy.text.toString() != ""
+                && binding.editAutonnomy.text.toString().toFloat() != 0f)
+    }
+
+    private fun calculate() {
+
+        if (isValidate()) {
+
+
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonnomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
